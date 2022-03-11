be7545ad1963e0ff5065f9baf9e576c9582e6a08
Tiago Luiz Pereira <tiago.luizsap@gmail.com>
2022-03-11 19:52:19 -0300

    commit inicial

---
+++ src/main/java/com/engsoft21/com/engsoft21/Agricultura21Application.java
@@ -0,0 +1,13 @@
+package com.engsoft21.com.engsoft21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agricultura21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agricultura21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/engsoft21/com/engsoft21/Agricultura21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engsoft21.com.engsoft21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agricultura21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
